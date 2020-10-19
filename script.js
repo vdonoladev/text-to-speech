ccdb71b6ab05ef338dedf58fcbe283786e598a0a
vdonoladev <victordonolaf@gmail.com>
2020-10-19 08:31:26 -0300

    Updates

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 window.SpeechRecognition =
-  window.SpeechRecognition ||
-  window.webkitSpeechRecognition;
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 // This allows live transcription. If it was false,
 //you would have to wait until done speaking to see the results.
 
 recognition.interimResults = true;
-recognition.lang = "en-US";
+recognition.lang = 'en-US';
 
-let p = document.createElement("p");
-const words = document.querySelector(".words");
+let p = document.createElement('p');
+const words = document.querySelector('.words');
 words.appendChild(p);
 
-recognition.addEventListener("result", (e) => {
+recognition.addEventListener('result', (e) => {
   const transcript = Array.from(e.results)
     .map((result) => result[0])
     .map((result) => result.transcript)
-    .join("");
+    .join('');
 
   p.textContent = transcript;
   if (e.results[0].isFinal) {
-    p = document.createElement("p");
+    p = document.createElement('p');
     words.appendChild(p);
   }
 });
 
-recognition.addEventListener("end", recognition.start);
+recognition.addEventListener('end', recognition.start);
 recognition.start();
